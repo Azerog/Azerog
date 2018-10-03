e198c4983a3190701058fcec84929592df41d4f5
Azerog <43039977+Azerog@users.noreply.github.com>
2018-10-03 12:26:45 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,18 +1,23 @@
-class Main {
-  public static void main(String[] args) {
-    int number=3;
-    if(number > 0)
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String[] args) 
     {
-      System.out.println(number+" is a posstive number");
+        int n;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter the number you want to check:");
+        n = s.nextInt();
+        if(n > 0)
+        {
+            System.out.println("The given number "+n+" is Positive");
+        }
+        else if(n < 0)
+        {
+            System.out.println("The given number "+n+" is Negative");
+        }
+        else
+        {
+            System.out.println("The given number "+n+" is neither Positive nor Negative ");
+        }
     }
-    else if(number < 0)
-    {
-      System.out.println(number+" number is negative");
-    }
-    else
-    {
-      System.out.println(number+" is neither positive nor negative");
-    }
-
-  }
 }
